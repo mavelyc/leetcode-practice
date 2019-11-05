4ee914e8d6004c3984433c5f3946368311935dba
mavelyc <cmavely@gmail.com>
2019-11-05 16:14:54 -0500

    added iteratively to reverse linked list

--- python/reverse-linked-list.py
+++ python/reverse-linked-list.py
@@ -6,9 +6,23 @@
 
 class Solution:
     def reverseList(self, head):
+
+        ########### Recusively ###################
         if not head or not head.next:
             return head
         p = self.reverseList(head.next)
         head.next.next = head
         head.next = None
         return p
+
+        ############ Iteratively ################
+        # curr = head
+        # prev = None
+        
+        # while curr != None:
+        #     tmp = curr.next
+        #     curr.next = prev
+        #     prev = curr
+        #     curr = tmp
+        
+        # return prev
