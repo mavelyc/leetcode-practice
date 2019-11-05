67fdaaf14b98bca5c2fb7f4d631a6e52caa2680c
mavelyc <cmavely@gmail.com>
2019-11-05 15:26:59 -0500

    reverse linked list

--- python/find-all-numbers-disappeared.py
+++ python/find-all-numbers-disappeared.py
@@ -1,3 +1,3 @@
 class Solution:
-    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+    def findDisappearedNumbers(self, nums):
         return set([x for x in range(1,len(nums)+1)]) - set(nums)
