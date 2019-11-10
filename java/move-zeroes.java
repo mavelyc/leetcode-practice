3ee692350613cace9f7dde20acdee78adf8d0380
mavelyc <cmavely@gmail.com>
2019-11-09 20:56:34 -0500

    move zeroes sol

---
+++ java/move-zeroes.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Solution {
+    public void moveZeroes(int[] nums) {
+        
+        int zeroCount = 0;
+        int i = 0;
+        
+        while (i<nums.length) {
+            if (nums[i] == 0) {
+                zeroCount++;
+            } else if (zeroCount > 0) {
+                nums[i-zeroCount] = nums[i];
+                nums[i] = 0;
+            }
+            i++;
+        }
+    }
+}
