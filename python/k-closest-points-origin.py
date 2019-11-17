a6d35d9e7ff0d1f3b0420b0f3304d7f0c12a47fb
mavelyc <cmavely@gmail.com>
2019-11-17 18:48:49 -0500

    k closest points to origin ineff sol

---
+++ python/k-closest-points-origin.py
@@ -0,0 +1,11 @@
+from collections import defaultdict
+import heapq
+class Solution(object):
+    def kClosest(self, points, K):
+        """
+        :type points: List[List[int]]
+        :type K: int
+        :rtype: List[List[int]]
+        """
+        points.sort(key = lambda x: x[0]**2+x[1]**2)
+        return points[:K]
