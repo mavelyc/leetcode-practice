2a9159b0cbe8ec8f748514966b73091cbc12f30e
mavelyc <cmavely@gmail.com>
2019-11-17 20:26:37 -0500

    del sol

--- python/amazon-warehouse-transport.py
+++
@@ -1,9 +0,0 @@
-import collections
-class Solution:
-    def __init__(self,totalCrates, allLocations, trunkCapacity):
-        self.main(totalCrates, allLocations, trunkCapacity)
-        
-    def main(totalCrates, allLocations, trunkCapacity):
-        
-
-test = Solution(90, [1,10,25,35,60])
