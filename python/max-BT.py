82b76250d91819a726250267713afcd6c2b2da08
mavelyc <cmavely@gmail.com>
2019-12-23 12:55:58 -0500

    max bt sol update

---
+++ python/max-BT.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def constructMaximumBinaryTree(self, nums: List[int]) -> TreeNode:
+        return self.construct(nums)
+        
+    def construct(self, nums):
+        max_i = nums.index(max(nums))
+        root = TreeNode(nums[max_i])
+        if len(nums[max_i+1:]) > 0:
+            root.right = self.construct(nums[max_i+1:])
+        if len(nums[:max_i]) > 0:
+            root.left = self.construct(nums[:max_i])
+        return root
